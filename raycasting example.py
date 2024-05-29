5f13818e22b3ab533bfa8351d6254a7bc36af878
tea-enjoyer11 <shqoographics@gmail.com>
2024-05-29 18:21:11 +0200

    raycasting v2

--- raycasting example.py
+++ raycasting example.py
@@ -1,3 +1,4 @@
+# https://www.youtube.com/watch?v=TOEi6T2mtHo
 from Scripts.utils_math import dist
 import math
 import pygame
@@ -17,9 +18,13 @@ class Boundary:
 
 
 class Ray:
-    def __init__(self, pos: Vector2, angle: float) -> None:
+    def __init__(self, pos: Vector2, dir: Vector2) -> None:
         self.pos = pos
-        self.dir = Vector2(math.cos(angle), math.sin(angle)) * 10
+        # self.dir = Vector2(math.cos(angle), math.sin(angle)) * 10
+        self.dir = dir
+        if self.dir.length() != 0:
+            self.dir = self.dir.normalize()
+        self.angle = math.atan2(self.dir.y, self.dir.x) % 360
 
     def set_pos(self, pos: Vector2):
         self.pos = pos
@@ -62,8 +67,16 @@ class Particle:
         self.pos = pos
         self.rays: list[Ray] = []
 
-        for a in range(0, 360, 10):
-            self.rays.append(Ray(self.pos, math.radians(a)))
+    def update_rays(self, walls: list[Boundary]):
+        self.rays = []
+        for b in walls:
+            self.rays.append(Ray(self.pos, (b.p1 - self.pos - Vector2(0.00001))))
+            self.rays.append(Ray(self.pos, (b.p1 - self.pos)))
+            self.rays.append(Ray(self.pos, (b.p1 - self.pos + Vector2(0.00001))))
+
+            self.rays.append(Ray(self.pos, (b.p2 - self.pos - Vector2(0.00001))))
+            self.rays.append(Ray(self.pos, (b.p2 - self.pos)))
+            self.rays.append(Ray(self.pos, (b.p2 - self.pos + Vector2(0.00001))))
 
     def cast(self, walls: list[Boundary]) -> list[Vector2]:
         l = []
@@ -79,8 +92,10 @@ class Particle:
                         closest = p
 
             if closest:
-                l.append(closest)
-        return l
+                l.append((closest, r.angle))
+
+        l = sorted(l, key=lambda x: x[1])
+        return [point for point, angle in l]
 
     def render(self, screen: Surface):
         pygame.draw.circle(screen, "red", self.pos, 5)
@@ -92,7 +107,9 @@ class Particle:
 
 
 walls = [Boundary(Vector2(random.randint(20, 380), random.randint(20, 380)), Vector2(random.randint(20, 380), random.randint(20, 380))) for _ in range(5)]
+walls.extend([Boundary(Vector2(0, 0), Vector2(0, 400)), Boundary(Vector2(0, 400), Vector2(400, 400)), Boundary(Vector2(400, 400), Vector2(400, 0)), Boundary(Vector2(400, 0), Vector2(0, 0))])
 part = Particle(Vector2(100, 200))
+part.update_rays(walls)
 
 while True:
     dt = clock.tick(0) * 0.001
@@ -103,10 +120,18 @@ while True:
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                walls = [Boundary(Vector2(random.randint(20, 380), random.randint(20, 380)), Vector2(random.randint(20, 380), random.randint(20, 380))) for _ in range(5)]
+                walls.extend([Boundary(Vector2(0, 0), Vector2(0, 400)), Boundary(Vector2(0, 400), Vector2(400, 400)), Boundary(Vector2(400, 400), Vector2(400, 0)), Boundary(Vector2(400, 0), Vector2(0, 0))])
 
     part.move(mpos)
+    part.update_rays(walls)
 
-    for p in part.cast(walls):
+    intersects = part.cast(walls)
+    pygame.draw.polygon(screen, (50, 50, 50), intersects)
+
+    for p in intersects:
         pygame.draw.circle(screen, "blue", p, 4)
         pygame.draw.line(screen, "yellow", part.pos, p, 2)
 
@@ -114,4 +139,7 @@ while True:
     part.render(screen)
     pygame.display.flip()
 
-# https://youtu.be/TOEi6T2mtHo?t=1700
+    pygame.display.set_caption(f"{clock.get_fps():.0f}")
+
+#! Blog über perfektes Raycasting:
+#! https://ncase.me/sight-and-light/
